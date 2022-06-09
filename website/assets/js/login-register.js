a0b82b39c1846b6b9ec316aefc3a307a16733a1d
underline <acs109153@gm.ntcu.edu.tw>
2022-06-09 16:28:54 +0800

    finished reCHAPTCHA function

---
+++ website/assets/js/login-register.js
@@ -0,0 +1,14 @@
+function error(err) 
+{
+    window.alert('reCAPTCHA 驗證失敗')
+}
+
+function registerVerifyCallback()
+{
+    document.getElementById("register").disabled = false;
+}
+
+function loginVerifyCallback()
+{
+    document.getElementById("login").disabled = false;
+}
