5a61e22a8c6ad8baf88623e196bf05b3c1894e17
underline <acs109153@gm.ntcu.edu.tw>
2022-06-17 21:18:46 +0800

    add insert_data_into_shopping_cart.php

---
+++ website/php/insert_data_into_shopping_cart.php
@@ -0,0 +1,30 @@
+<?php
+//加入購物車 要輸入2個參數：$_GET['Goods_ID'], $_GET[num]
+session_start();
+include('../connect-sql.php');
+$sql = "SELECT * FROM `goods` WHERE `Goods_ID` = " . $_GET['Goods_ID'] . ";";
+$result = mysqli_query($db_link, $sql);
+$row = $result->fetch_assoc();
+
+$Goods_ID = $row['Goods_ID'];
+$Goods_Name = $row['Goods_Name'];
+$Goods_Price = $row['Goods_Price'];
+$Goods_Num = $_GET['num'];
+$Goods_URL = $row['Goods_URL'];
+$Goods_Statement = nl2br($row['Goods_Statement']);
+$Goods_Classify = $row['Goods_Classify'];
+$Goods_Specs = nl2br($row['Goods_Specs']);
+$Buyer_ID = $_SESSION['id'];
+
+$sql = 'INSERT INTO `cus_shopping_cart` (`Goods_ID`, `Goods_Name`, `Goods_Price`, `Goods_Num`, `Goods_URL`, `Buyer_Record_ID`) VALUES 
+("' . $Goods_ID . '","' . $Goods_Name . '",' . $Goods_Price . ',' . $Goods_Num . ',"' . $Goods_URL . '","' . $Buyer_ID . '")';
+echo $sql;
+if (mysqli_query($db_link, $sql)) 
+{
+    header("location:../cart.php");
+} 
+else 
+{
+    echo "Error: " . $sql . "<br>" . mysqli_error($db_link);
+}                                  
+?>
