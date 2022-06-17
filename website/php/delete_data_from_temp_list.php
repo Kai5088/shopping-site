e65f1612172840da4a6b9b2fceb7ffaf10c5057a
underline <acs109153@gm.ntcu.edu.tw>
2022-06-17 17:16:51 +0800

    add delete_data_from_temp_list.php

---
+++ website/php/delete_data_from_temp_list.php
@@ -0,0 +1,14 @@
+<?php
+//從暫存清單中刪除資料 要輸入2個參數：$_GET['Cus_ID'], $_GET['Goods_ID']
+session_start();
+include('../connect-sql.php');
+$sql = "DElETE FROM `cus_temp_list` WHERE `Goods_ID` = " . $_GET['Goods_ID'] . " AND `Cus_ID` = '" . $_GET['Cus_ID'] . "';";
+if (mysqli_query($db_link, $sql)) 
+{
+    header("location:../wishlist.php");
+} 
+else 
+{
+    echo "Error: " . $sql . "<br>" . mysqli_error($db_link);
+}                                  
+?>
