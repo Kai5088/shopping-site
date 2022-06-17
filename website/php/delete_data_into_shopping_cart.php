77c1c056ecb4d5796c8461358c6b605d5f1b7618
underline <acs109153@gm.ntcu.edu.tw>
2022-06-17 22:12:58 +0800

    add delete_data_into_shopping_cart.php

---
+++ website/php/delete_data_into_shopping_cart.php
@@ -0,0 +1,15 @@
+<?php
+//從購物車中刪除資料 要傳入1個參數：$_GET['Goods_ID']
+session_start();
+include('../connect-sql.php');
+$Cus_ID = $_SESSION['id'];
+$sql = "DElETE FROM `cus_shopping_cart` WHERE `Goods_ID` = " . $_GET['Goods_ID'] .  " AND `Cus_ID` = '" . $Cus_ID . "';";
+if (mysqli_query($db_link, $sql)) 
+{
+    header("location:../cart.php");
+} 
+else 
+{
+    echo "Error: " . $sql . "<br>" . mysqli_error($db_link);
+}                                  
+?>
