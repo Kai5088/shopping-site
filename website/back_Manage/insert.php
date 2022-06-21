2717f451c0b03f87f099b4d0621959d444b8bb08
YANG JYUN-KAI <acs109151@gm.ntcu.edu.tw>
2022-06-21 16:16:54 +0000

    Replace insert.php

--- website/back_Manage/insert.php
+++ website/back_Manage/insert.php
@@ -1,12 +1,7 @@
 <?php
 require_once('conn.php');
-$S=trim($_POST['select1']);
-if (preg_match("/\新增商品分類/i", $_POST['select2'])){
-	$S2=trim($_POST['select8']);
-}
-else{
-	$S2=trim($_POST['select2']);	
-}
+$S1=trim($_POST['select1']);
+$S2=trim($_POST['select2']);
 $S3=trim($_POST['select3']);
 $S4=trim($_POST['select4']);
 $S5=trim($_POST['select5']);
@@ -14,7 +9,7 @@ $S6=trim($_POST['select6']);
 $S7=trim($_POST['select7']);
 //刪除資料表中的資料
 $sql = "INSERT INTO goods (Goods_Classify, Goods_Name,Goods_Num,Goods_Price,Goods_Specs,Goods_Statement,Goods_URL)
-VALUES ('$S', '$S2', '$S3','$S4','$S5','$S6','$S7');";
+VALUES ('$S1', '$S2', '$S3','$S4','$S5','$S6','$S7');";
 mysqli_query($conn,$sql);
 header( "location:manager.php"); 
 ?>
